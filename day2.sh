b90da2c7bbe88b315beab8dda8c93ba9658734e0
Geek <alfredybruno@gmail.com>
2020-02-20 22:27:44 +0300

    more development on day2.sh

--- day2.sh
+++ day2.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-SKILL="shell scripting"
-echo "I want to be good at ${SKILL}, That's why i practice ${SKILL}"
+echo -n "Enter the animal name: "
+read animal
+echo -n "The $animal has "
+case $animal in
+    horse | dog | cat ) echo -n "four" ;;
+    human | kangaroo ) echo -n "two" ;;
+    * ) echo -n "unknown number of " ;;
+esac
+echo legs
